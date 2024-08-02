8d3d8e114f4787ddd7e0529da039a7e59cd4f057
PaijeEllis <paijeellis517@gmail.com>
2024-08-02 14:36:24 +0000

    Learning about regular expression

---
+++ complexlab/superhero.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""RZfeeser | Paije E
+   Superhero programmers"""
+
+
+# name in dictionary
+
+classinfo= [{"name": "Paije",
+            "skill level": "phenomenal",
+            "spirit animal": "Leopard",
+            "super power": "Stretchy",
+            }]
+
+print(classinfo)
